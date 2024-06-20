63aaca61695361694fbdd7b5adeb46bf58595e29
Innocentia Dutoit <innocentia.dutoit9140@gmail.com>
2024-06-20 13:04:09 +0200

    Created index file

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+  echo "Hello, World!<br>";
+  echo "Current date and time: " . date('y-m-d H:i:s');
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title></title>
+</head>
+<body>
+     <a href="authors.php">Authors</a>
+</body>
+</html>
