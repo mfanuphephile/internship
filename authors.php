9e6b7f763764d98e7550aad2e4dc5858ad2c15be
mfanuphephile mnisi <mfanuphephile.mnisi9150@gmail.com>
2024-06-20 14:45:24 +0200

    cleanup

--- authors.php
+++ authors.php
@@ -8,7 +8,7 @@ $authors_response = $main->getAuthors();
 if($authors_response){
 	$counter = 1;
 	while($author = $authors_response->fetch_assoc()){
-		echo "Author $counter <br>Name: ".$author['name']." Email: ".$author['email'];
+		echo "Author $counter <br>Name: ".$author['name']." Email: ".$author['email']."<br>";
 	}
 }else{
 	echo "Failed to get authors";
