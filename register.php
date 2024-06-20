ff48a74d36d3042a6918083fa386ba6ecf30fcf7
Innocentia Dutoit <innocentia.dutoit9140@gmail.com>
2024-06-20 13:55:26 +0200

    created form

---
+++ register.php
@@ -0,0 +1,24 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Register</title>
+</head>
+<body> 
+
+	<form action="endpoint.php" method="POST">
+		
+	 <label>Name</label>
+	 <input type="text" name="name">
+
+	  <label>Email</label>
+	 <input type="text" name="email">
+
+	 <button type="register">SUBMIT</button>
+
+	</form>
+
+</body>
+</html>
